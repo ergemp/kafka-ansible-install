80c6d2f4e2785cf6f5ee8d508699ae9354c81ca7
ergemp <ergemp@gmail.com>
2023-07-10 15:24:40 +0300

    init

---
+++ configs/kafka/kafka-server-start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export JMX_PORT=9999
+export KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname={{ ip_address }} -Djava.net.preferIPv4Stack=true"
+export KAFKA_OPTS="-javaagent:/usr/local/jmx-exporter/jmx_prometheus_javaagent-0.19.0.jar=8081:/usr/local/jmx-exporter/kafka-2_0_0.yml"
+
+
+if [ $# -lt 1 ];
+then
+	echo "USAGE: $0 [-daemon] server.properties [--override property=value]*"
+	exit 1
+fi
+base_dir=$(dirname $0)
+
+if [ "x$KAFKA_LOG4J_OPTS" = "x" ]; then
+    export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:$base_dir/../config/log4j.properties"
+fi
+
+if [ "x$KAFKA_HEAP_OPTS" = "x" ]; then
+    export KAFKA_HEAP_OPTS="-Xmx1G -Xms1G"
+fi
+
+EXTRA_ARGS=${EXTRA_ARGS-'-name kafkaServer -loggc'}
+
+COMMAND=$1
+case $COMMAND in
+  -daemon)
+    EXTRA_ARGS="-daemon "$EXTRA_ARGS
+    shift
+    ;;
+  *)
+    ;;
+esac
+
+exec $base_dir/kafka-run-class.sh $EXTRA_ARGS kafka.Kafka "$@"
